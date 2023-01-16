2ca4e8d1c3d285701b9bed923046f852acae24a6
kaivalmehta <kaivalhmehta@gmail.com>
2023-01-16 23:26:38 +0530

    COMMIT-2

---
+++ Python_Practice2.py
@@ -0,0 +1,29 @@
+#Lists
+list1=[1,2,3,4,5]
+print(list1[3])
+print(list1[-2])
+print(list1[0:4])
+print(list1[4:0:-2])
+
+#Inputting a list
+list2=eval(input("Enter a List"))
+print(list2)
+
+#Tupples
+a=1,2,3
+print(a)
+
+#Set
+b={8,9,10}
+
+#Dictionary
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+print(thisdict["brand"])
+
+thisdict1 = dict(name = "John", age = 36, country = "Norway")
+print(thisdict1)
