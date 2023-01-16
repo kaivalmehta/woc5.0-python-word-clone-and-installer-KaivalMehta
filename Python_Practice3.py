3a42c8e2e0c3d48d16b534bfae979190f43c2da8
kaivalmehta <kaivalhmehta@gmail.com>
2023-01-16 23:32:18 +0530

    COMMIT-3

---
+++ Python_Practice3.py
@@ -0,0 +1,24 @@
+#IF-ELIF-ELSE
+a = 5
+b = 8
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else :
+  print("a is greater than b")
+#IN STATEMENTS
+a = 3
+b = 9
+print("A") if a > b else print("B")
+
+#NESTED
+x = 41
+
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  else:
+    print("but not above 20.")
+
